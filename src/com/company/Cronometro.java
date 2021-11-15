ad833947e7d9324a1f7178ed8c37185494770666
DanielAlvarez02 <55715973+DanielAlvarez02@users.noreply.github.com>
2021-11-14 19:24:24 -0500

    Cambios UML
    Cambios debido al diagrama de clases y secuencia UML

--- src/com/company/Cronometro.java
+++ src/com/company/Cronometro.java
@@ -7,25 +7,25 @@ public class Cronometro {
     Timer temporizador = new Timer(); // nos permite contar intervalos de tiempo
     TareaDeTemporizador iterador; //nos permite hacer algo después de cierto intervalo de tiempo
 
-    int tiempoDeActividad = 5; // tiempo de actividad en segundos
-    int tiempoDeDescansoCorto = 1; // tiempo de descanso corto en segundos
-    int tiempoDeDescansoLargo = 3; // tiempo de descanso largo en segundos
-    int numeroDeCiclosParaDescansoLargo = 4; //Numero de ciclos que deben pasar para que se ejecute el tiempo largo de espera
+    private int tiempoDeActividad = 5; // tiempo de actividad en segundos
+    private int tiempoDeDescansoCorto = 1; // tiempo de descanso corto en segundos
+    private int tiempoDeDescansoLargo = 3; // tiempo de descanso largo en segundos
+    private int numeroDeCiclosParaDescansoLargo = 4; //Numero de ciclos que deben pasar para que se ejecute el tiempo largo de espera
 
     //Cuando se pausa el cronómetro debe crearse un nuevo iterador, por lo tanto se crearon las siguientes variables
     //para conservar el estado del objeto iterador.
-    int segundosRestantesActuales = tiempoDeActividad;
-    int cantidadDeCiclosActuales = 0;
+    private int segundosRestantesActuales = tiempoDeActividad;
+    private int cantidadDeCiclosActuales = 0;
     FaseDelPomodoro fase = FaseDelPomodoro.actividad;
 
     //Se inicia el cronómetro, creando un iterador y haciendo que se corra cada 1000ms (1 segundo)
-    public void iniciar(){
+    public void iniciarCiclos(){
         iterador= new TareaDeTemporizador(segundosRestantesActuales, cantidadDeCiclosActuales, fase);
         temporizador.scheduleAtFixedRate(iterador, 0, 1000);
     }
 
     //Se extrae el estado del iterador y postariormente se elimina del temporizador.
-    public void pausar(){
+    public void pausarCiclos(){
         this.segundosRestantesActuales = iterador.getSegundosRestantes();
         this.cantidadDeCiclosActuales = iterador.getCantidadDeCiclos();
         this.fase = iterador.getFase();
@@ -34,8 +34,24 @@ public class Cronometro {
         iterador = null;
     }
 
+    public void modificarTiempoDeActividad(int tiempoDeActividad) {
+        this.tiempoDeActividad = tiempoDeActividad;
+    }
+
+    public void modificarTiempoDeDescansoCorto(int tiempoDeDescansoCorto) {
+        this.tiempoDeDescansoCorto = tiempoDeDescansoCorto;
+    }
+
+    public void modificarTiempoDeDescansoLargo(int tiempoDeDescansoLargo) {
+        this.tiempoDeDescansoLargo = tiempoDeDescansoLargo;
+    }
+
+    public void modificarNumeroDeCiclosParaDescansoLargo(int numeroDeCiclosParaDescansoLargo) {
+        this.numeroDeCiclosParaDescansoLargo = numeroDeCiclosParaDescansoLargo;
+    }
+
     //Clase que tiene las acciones que van a ser ejecutadas por el Cronómetro
-    class TareaDeTemporizador extends TimerTask {
+    private class TareaDeTemporizador extends TimerTask {
         //El estado de esta clase se conforma del valor de sus atributos
         int segundosRestantes = tiempoDeActividad; //Segundos restantes del ciclo
         int cantidadDeCiclos = 0; // Cantidad de ciclos transcurridos
@@ -44,9 +60,9 @@ public class Cronometro {
         // Constructor que toma como entrada, los segundos restantes, cantidad de ciclos y fase
         // Este objeto sirve para crear un nuevo objeto con el mismo estado de otro objeto
         public TareaDeTemporizador(int segundosRestantes, int cantidadDeCiclos, FaseDelPomodoro fase){
-            this.segundosRestantes = segundosRestantes;
-            this.cantidadDeCiclos = cantidadDeCiclos;
-            this.fase = fase;
+                this.segundosRestantes = segundosRestantes;
+                this.cantidadDeCiclos = cantidadDeCiclos;
+                this.fase = fase;
         }
 
         //Este es el método que el cronómetro ejecuta cada 1 segundo
@@ -125,7 +141,7 @@ public class Cronometro {
     }
 
     //fases del pomodoro: actividad, descanso corto y descanso largo
-    enum FaseDelPomodoro{
+    private enum FaseDelPomodoro{
         actividad, desansoCorto, descansoLargo;
     }
 }
