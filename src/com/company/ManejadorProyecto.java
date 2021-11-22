bf289ed0e45bb3857416b399b8984b4bcc6a25ad
DanielAlvarez02 <55715973+DanielAlvarez02@users.noreply.github.com>
2021-11-21 23:38:46 -0500

    Refactors 4 - 9

--- src/com/company/ManejadorProyecto.java
+++ src/com/company/ManejadorProyecto.java
@@ -2,32 +2,19 @@ package com.company;
 import java.util.*;
 
 public class ManejadorProyecto {
-    private LinkedList <Proyecto> lista;
+    private LinkedList <Proyecto> proyectos;
     Cronometro pomodoro = new Cronometro();
 
     public ManejadorProyecto() {
-        lista = new LinkedList<Proyecto>();
+        proyectos = new LinkedList<Proyecto>();
     }
 
     public void agregarProyecto(Proyecto proyecto){
-        lista.add(proyecto);
+        proyectos.add(proyecto);
     }
-    public void abrirProyecto(){
 
-        Scanner sn = new Scanner(System.in);
-        while(true) {
-            System.out.println("A continuación se muestran todos los proyecto: \n");
-            System.out.println(lista.toString());
-            System.out.println("Por favor ingrese el nombre del proyecto que desea abrir o presione q para salir: \n");
-            String tema = sn.next();
-            if(tema.equals("q")) return;
-            Proyecto proyectoEncontrado = buscarProyecto(tema);
-            if (proyectoEncontrado != null) proyectoEncontrado.menuProyecto();
-            else System.out.println("ingrese un nombre valido o q para salir");
-        }
-    }
     public Proyecto buscarProyecto(String nombreProyecto){
-        for(Proyecto proy : this.lista){
+        for(Proyecto proy : this.proyectos){
             if(proy.getTema().equals(nombreProyecto)){
                 return proy;
             }
@@ -35,7 +22,7 @@ public class ManejadorProyecto {
         return null;
     }
     public void eliminarProyecto(String nombreProyecto){
-        Iterator <Proyecto>it = lista.iterator();
+        Iterator <Proyecto>it = proyectos.iterator();
         while(it.hasNext()){
             String tema = it.next().getTema();
             if(tema.equalsIgnoreCase(nombreProyecto)){
@@ -47,5 +34,9 @@ public class ManejadorProyecto {
 
     public Cronometro getPomodoro() {
         return pomodoro;
+    }
+
+    public LinkedList<Proyecto> getProyectos() {
+        return proyectos;
     }
 }
