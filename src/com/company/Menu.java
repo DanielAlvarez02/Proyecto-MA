cc94cfa659c99defdae5e24b98ad13a62409b482
DanielAlvarez02 <55715973+DanielAlvarez02@users.noreply.github.com>
2021-11-21 22:31:14 -0500

    Extract class
    Extract menu methods in main to class Menú

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -6,5 +6,37 @@ public class Menu {
     ManejadorProyecto manejador;
     Scanner sc = new Scanner(System.in);
 
+    public Menu(){
+        Scanner sc = new Scanner(System.in);
+        ManejadorProyecto manejador = new ManejadorProyecto();
+        System.out.println("Agregue un proyecto para empezar");
+        System.out.println("Ingrese un tema para el proyecto: ");
+        String tema = sc.next();
+        manejador.agregarProyecto(new Proyecto(tema));
+        menuPrincipal(manejador);
+    }
+
+    public void menuPrincipal(ManejadorProyecto manejador){
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while(true) {
+            while (true) {
+                System.out.println("Seleccione que desea hacer:" +
+                        "\n1. ver proyectos" +
+                        "\n2. modificar pomodoro" +
+                        "\n3. salir");
+                input = sc.next();
+                if (input.equals("1") || input.equals("2") || input.equals("3")) break;
+                System.out.println("Input incorrecto, seleccione 1, 2 o 3");
+            }
+            if (input.equals("1")) {
+                manejador.abrirProyecto();
+            } else if(input.equals("2")){
+                manejador.editarPomodoro();
+            }
+            else System.exit(1);
+        }
+    }
+
 
 }
