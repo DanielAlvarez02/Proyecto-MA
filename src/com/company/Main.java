c05aa38986c333db75a0f99d011fa7148529ddb2
DanielAlvarez02 <55715973+DanielAlvarez02@users.noreply.github.com>
2021-11-21 21:39:31 -0500

    Main update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,16 @@
 package com.company;
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        ManejadorProyecto manejador = new ManejadorProyecto();
+        System.out.println("Agregue un proyecto para empezar");
+        System.out.println("Ingrese un tema para el proyecto: ");
+        String tema = sc.nextLine();
+        manejador.agregarProyecto(new Proyecto(tema));
     }
 }
