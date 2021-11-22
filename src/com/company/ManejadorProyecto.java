091391c3b74954cc37f981dd23cfb7fde268823a
DanielAlvarez02 <55715973+DanielAlvarez02@users.noreply.github.com>
2021-11-21 22:19:33 -0500

    Update ManejadorProyecto.java
    Buscar arreglado

--- src/com/company/ManejadorProyecto.java
+++ src/com/company/ManejadorProyecto.java
@@ -13,26 +13,26 @@ public class ManejadorProyecto {
         lista.add(proyecto);
     }
     public void abrirProyecto(){
-        Scanner sn = new Scanner(System.in);
-        System.out.println("A continuación se muestran todos los proyecto: \n");
-        System.out.println(lista.toString());
-        System.out.println("Por favor ingrese el nombre del proyecto que desea abrir: \n");
-        String tema = sn.next();
-        for(Proyecto aux: lista){
-            if(aux.getTema().equalsIgnoreCase(tema)){
-                aux.toString();
-            }
-        }
 
+        Scanner sn = new Scanner(System.in);
+        while(true) {
+            System.out.println("A continuación se muestran todos los proyecto: \n");
+            System.out.println(lista.toString());
+            System.out.println("Por favor ingrese el nombre del proyecto que desea abrir o presione q para salir: \n");
+            String tema = sn.next();
+            if(tema.equals("q")) return;
+            Proyecto proyectoEncontrado = buscarProyecto(tema);
+            if (proyectoEncontrado != null) proyectoEncontrado.menuProyecto();
+            else System.out.println("ingrese un nombre valido o q para salir");
+        }
     }
-    public void buscarProyecto(String nombreProyecto){
-        Iterator <Proyecto>it = lista.iterator();
-        while(it.hasNext()){
-            String tema = it.next().getTema();
-            if(tema.equalsIgnoreCase(nombreProyecto)){
-                it.toString();
+    public Proyecto buscarProyecto(String nombreProyecto){
+        for(Proyecto proy : this.lista){
+            if(proy.getTema().equals(nombreProyecto)){
+                return proy;
             }
         }
+        return null;
     }
     public void eliminarProyecto(String nombreProyecto){
         Iterator <Proyecto>it = lista.iterator();
@@ -41,7 +41,6 @@ public class ManejadorProyecto {
             if(tema.equalsIgnoreCase(nombreProyecto)){
                 it.remove();
             }
-
         }
     }
     public void editarPomodoro(){
