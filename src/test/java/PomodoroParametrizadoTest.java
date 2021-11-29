158cc875490d17fc0026de2451e0cb15e82f1752
Aiman <55715766+Danieloch14@users.noreply.github.com>
2021-11-28 21:04:25 -0500

    Parametrizada test
    1 PARAMETRIZADA TEST

--- src/test/java/PomodoroParametrizadoTest.java
+++ src/test/java/PomodoroParametrizadoTest.java
@@ -1,3 +1,5 @@
+import com.company.Cronometro;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -5,12 +7,31 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(value = Parameterized.class)
 public class PomodoroParametrizadoTest {
-    private int a, b, expected;
+    private int expected;
     public static Iterable<Object[]> parameters(){
         List<Object[]> objects = new ArrayList<Object[]>();
-        objects.add(new Object[]{});
+        objects.add(new Object[]{2});
+        objects.add(new Object[]{8});
+        objects.add(new Object[]{9});
+        objects.add(new Object[]{7});
+        objects.add(new Object[]{1});
+        objects.add(new Object[]{5});
+        objects.add(new Object[]{3});
+        return objects;
+    }
 
+    @Test
+    public void give_same_Cronometro_when_numeroDeCiclos_modified_then_ok() {
+        Cronometro pomodoro = new Cronometro();
+        int nCEsperado = 2;
+
+        pomodoro.modificarNumeroDeCiclosParaDescansoLargo(nCEsperado);
+        assertEquals(nCEsperado, pomodoro.getNumeroDeCiclosParaDescansoLargo());
+
+        //HEAD
     }
 }
