febc35a4e78ab7107bd1305bac7877ff9dae340e
DanielAlvarez02 <55715973+DanielAlvarez02@users.noreply.github.com>
2021-11-28 20:46:05 -0500

    segundos restantes updated

--- src/main/java/com/company/Cronometro.java
+++ src/main/java/com/company/Cronometro.java
@@ -134,7 +134,7 @@ public class Cronometro {
         return faseDelPomodoroActual;
     }
 
-    public int getSegundosRestantes() {
+    public int getSegundosRestantesActuales() {
         return segundosRestantes;
     }
 
